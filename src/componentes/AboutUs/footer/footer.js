4ee68206b1057c79dcf949313fa3b93a3502e296
IreneWallfall <irenewaraya09@gmail.com>
2023-10-01 00:13:25 -0600

    Contenido Footer
    Se agregaron los distintos elementos del footer.

--- src/componentes/AboutUs/footer/footer.js
+++ src/componentes/AboutUs/footer/footer.js
@@ -1,9 +1,29 @@
 import React from "react";
+import './styleFooter.css'
 
 const FooterAboutUs = () => {
     return(
         <div>
-            <p>...</p>
+            <section className="menuItems">
+                <ul>
+                    <li><a href="#"> Find your recipe</a></li>
+                    <li><a href="#">Ideas</a></li> 
+                   <li> <a href="#">Favorites</a></li>
+                    <li> <a href="#">About Us</a></li>
+                    <li> <a href="#">Sign Up</a></li>
+                </ul>
+            </section>
+            <section className="containerInfo">
+                {/* <section className="iconSection">
+                    <a href="#"><Icon.Twitter size="25" className="icon"/></a>
+                    <a href="#"><Icon.Instagram size="25" className="icon"/></a>
+                    <a href="#"><Icon.Facebook size="25" className="icon"/></a>
+                    <a href="#"><Icon.Tiktok size="25" className="icon"/></a>
+                    <a href="#"><Icon.Youtube size="25" className="icon"/></a>
+                    <a href="#"><Icon.Pinterest size="25" className="icon"/></a>
+                </section> */}
+                <p>© 2023.All rights reserved</p>
+            </section>
         </div>
     )
 }
