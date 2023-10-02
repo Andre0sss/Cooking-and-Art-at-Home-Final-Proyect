e555e90e142c9b73130d382bc380ba3c3732260a
IreneWallfall <irenewaraya09@gmail.com>
2023-10-01 22:27:26 -0600

    Contenido Contact
    Se agregaron los distintos elementos del componente y sus estilos

--- src/componentes/ContacInfo/Contact.js
+++ src/componentes/ContacInfo/Contact.js
@@ -1,11 +1,12 @@
 import React from "react";
+import * as Icon from 'react-bootstrap-icons';
 import './style.css';
 import './responsive.css';
 
 const ContactUs = () => {
     return(
-    <div className="container">
-        <div>
+    <div className="mainContainer">
+        <div className="containerForm">
             <h1>Problems with the kitchen?</h1>
             <form>
                 <input placeholder="Name" type="text"></input>
@@ -14,13 +15,22 @@ const ContactUs = () => {
                 <button>Submit</button>
             </form>
         </div>
-        <div>
-            <section></section>
-            <section></section>
-            <section></section>
-            <section></section>
-            <section></section>
-            <section></section>
+        <div className="containerInfo">
+            <Icon.HouseFill size="25"/>
+            <h2>Contact Info</h2>
+            <p><Icon.EnvelopeAtFill size="25"/> empresa@gmail.com</p>
+            <p><Icon.TelephoneFill size="25"/> +506 1234-5678</p>
+            <section>
+            <section className="iconSectionC">
+                    <a href="#"><Icon.Twitter size="25" className="icon"/></a>
+                    <a href="#"><Icon.Instagram size="25" className="icon"/></a>
+                    <a href="#"><Icon.Facebook size="25" className="icon"/></a>
+                    <a href="#"><Icon.Tiktok size="25" className="icon"/></a>
+                    <a href="#"><Icon.Youtube size="25" className="icon"/></a>
+                    <a href="#"><Icon.Pinterest size="25" className="icon"/></a>
+                </section>
+            </section>
+            <p>© 2023. All rights reserved</p>
         </div>
     </div>
     );
