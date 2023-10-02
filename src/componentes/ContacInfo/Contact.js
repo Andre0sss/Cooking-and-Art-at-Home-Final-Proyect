8e68229af2270157a734fc87f9d622d83eb2e3bd
IreneWallfall <irenewaraya09@gmail.com>
2023-10-01 21:12:19 -0600

    Contenido Contact Us

--- src/componentes/ContacInfo/Contact.js
+++ src/componentes/ContacInfo/Contact.js
@@ -1,3 +1,29 @@
 import React from "react";
-import './styles.css';
+import './style.css';
 import './responsive.css';
+
+const ContactUs = () => {
+    return(
+    <div className="container">
+        <div>
+            <h1>Problems with the kitchen?</h1>
+            <form>
+                <input placeholder="Name" type="text"></input>
+                <input placeholder="Email" type="email"></input>
+                <textarea placeholder="Message"></textarea>
+                <button>Submit</button>
+            </form>
+        </div>
+        <div>
+            <section></section>
+            <section></section>
+            <section></section>
+            <section></section>
+            <section></section>
+            <section></section>
+        </div>
+    </div>
+    );
+}
+
+export default ContactUs;
