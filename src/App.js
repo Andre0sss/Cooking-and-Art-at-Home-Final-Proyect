4aceae69bb7603f2b43ec63d0bcedbc7e6554dce
IreneWallfall <irenewaraya09@gmail.com>
2023-10-01 21:11:36 -0600

    Actualización App
    Se inserto el componente de ContactUs al App.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import AboutUs from './componentes/AboutUs/AboutUs';
 import AppFindRecepy from './componentes/FindRecepy/AppFindRecepy';
+import ContactUs from './componentes/ContacInfo/Contact';
 
 function App() {
   return (
     <div className="App">
       <AboutUs />
       <AppFindRecepy/>
+      <ContactUs /> 
     </div>
   );
 }
