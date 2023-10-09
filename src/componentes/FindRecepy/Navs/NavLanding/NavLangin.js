09f8ab0f85e87ab6f9f33f1ccc07cc6e36205367
Brayan Andres <mora76428@gmail.com>
2023-10-09 09:22:10 -0600

    cambios en el nav

--- src/componentes/FindRecepy/Navs/NavLanding/NavLangin.js
+++ src/componentes/FindRecepy/Navs/NavLanding/NavLangin.js
@@ -9,7 +9,7 @@ const NavLanding = ()=>{
                     <nav className="nav-text">
                         <ul>
                             <li>
-                                <Link to='/findRecepy'>Create Recipe</Link>
+                                <Link to='/findRecepy'>Find Recipe</Link>
                             </li>
                             <li>
                                 <Link to='/contacUs'>Contact Us</Link>
