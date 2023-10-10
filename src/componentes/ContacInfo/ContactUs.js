fb45ed407402b886f110daf4ae42a53ec0bdcf65
IreneWallfall <irenewaraya09@gmail.com>
2023-10-09 20:26:04 -0600

    Ajustes
    Ajustes en contactos y en el titulo del sitio

--- src/componentes/ContacInfo/ContactUs.js
+++ src/componentes/ContacInfo/ContactUs.js
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import * as Icon from 'react-bootstrap-icons';
 import './style.css';
 import './responsive.css';
 
 const ContactUs = () => {
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_gik3mn4', 'template_6mslavi', form.current, 'YRA8uzbrtPDfP5AdF')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+    };
+
     return(
     <div className="mainContainer">
         <div className="containerForm">
             <h1>Problems in the kitchen?</h1>
-            <form className="contactForm">
-                <input placeholder="Name" type="text"></input>
-                <input placeholder="Email" type="email"></input>
-                <textarea placeholder="Message"></textarea>
-                <button>Submit</button>
+            <form ref={form} onSubmit={sendEmail} className="contactForm">
+                <input placeholder="Name" type="text" name="user_name"></input>
+                <input placeholder="Email" type="email" name="user_email"></input>
+                <textarea placeholder="Message" name="message"></textarea>
+                <button type="submit" value="Send">Submit</button>
             </form>
         </div>
         <div className="containerInfo">
